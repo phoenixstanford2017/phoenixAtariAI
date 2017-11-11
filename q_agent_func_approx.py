43dad32d5b2c64d13558ac262644965d5ad4b63c
adiazraya <adiazraya@gmail.com>
2017-11-11 14:09:41 -0300

    add the program that run the learner and play and adpating the learner

--- q_agent_func_approx.py
+++ q_agent_func_approx.py
@@ -11,13 +11,15 @@ class QAgentFuncApprox(object):
     """
 
     def __init__(self, observation_space, action_space, **userconfig):
-        if not isinstance(observation_space, discrete.Discrete):
+        '''if not isinstance(observation_space, discrete.Discrete):
             raise UnsupportedSpace('Observation space {} incompatible with {}. (Only supports Discrete observation spaces.)'.format(observation_space, self))
+        
         if not isinstance(action_space, discrete.Discrete):
             raise UnsupportedSpace('Action space {} incompatible with {}. (Only supports Discrete action spaces.)'.format(action_space, self))
+        '''
         self.observation_space = observation_space
         self.action_space = action_space
-        self.action_n = action_space.n
+        self.action_n = len(action_space)
         self.eta = 0.1
         self.eps = 0.05
         self.discount = 0.95
@@ -39,30 +41,36 @@ class QAgentFuncApprox(object):
     # Note that if s is a terminal state, then s' will be None.  Remember to check for this.
     # You should update the weights using self.getStepSize(); use
     # self.getQ() to compute the current estimate of the parameters.
+    def noNewState(self, state):
+        return len(state)==0
     def incorporateFeedback(self, state, action, reward, newState):
         DeltaWeights=defaultdict(float)
         #for f,v in self.featureExtractor(state,action):
         #    self.weights[f]= 0 
         Prediction=self.getQ(state,action)
         eta=self.getStepSize()
-        if newState!=None:
+        if self.noNewState(state):
             V=[self.getQ(newState,newAction) for newAction in self.action_space]
             Vopt=max(V)
             Target=reward+self.discount*Vopt
         else:
             Target=reward
         DeltaScalar=self.eta*(Prediction-Target)
-        for key,v in self.feature_extractor(state,action).items():
+        for key,v in self.feature_extractor(state,action):
             DeltaWeights[key]= DeltaScalar * v 
         for key in self.weights.keys():
             self.weights[key]=self.weights.get(key,0)-DeltaWeights.get(key,0)
     # Define the feature extractor
     def feature_extractor(self, state, action):
-        phi = defaultdict(float)
-        phi[(state,action)] = 1
+        phi=[]
+        featureValue = 1
+        fk1 = (tuple(state), action)
+
+        phi.append((fk1,featureValue))
         for i, v in enumerate(state):
-            phi[(i, v, action)] = 1
-        print('phi for iteration: %s new phi is: %s' % (self.numIters, phi))
+            fk2=(i,v,action)
+            phi.append((fk2,featureValue))
+        #print('phi for iteration: %s new phi is: %s' % (self.numIters, phi))
         return phi
 
     def act(self, observation, eps=None):
@@ -70,7 +78,7 @@ class QAgentFuncApprox(object):
             eps = self.eps
         # epsilon greedy.
         if numpy.random.random() <= eps:
-            action=self.action_space.sample()
+            action=numpy.random.choice(self.action_space)
         else:
             action = max((self.getQ(observation, action), action) for action in self.action_space)[1] 
         return action
---
+++ run_q-agent.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+import gym
+from gym.spaces import discrete
+import gym.spaces
+env = gym.make('Phoenix-ram-v0')
+print env.observation_space
+print env.action_space
+    
+import   q_agent_func_approx 
+learner= q_agent_func_approx.QAgentFuncApprox(env.observation_space,[0,1,2,3,4,5,6,7])
+learner.learn(env)
+qOpt = defaultdict()
+
+'''
+for observation in env.observation_space:
+    action=learner.act(observation,eps=0)
+    qOpt[observation]=action
+'''
+
+
+tot_rewards = []
+quiet = False
+for i_episode in range(2):
+    observation = env.reset()
+    tot_reward = 0
+    print '##################'
+    for t in range(10000):
+        if not quiet:
+            env.render()
+        #action = qOpt[observation]
+        action=learner.act(observation,eps=0)
+        #print 'action: %s' % action
+        observation, reward, done, info = env.step(action)
+        if reward:
+            #print observation
+            print info
+            print reward
+            tot_reward += reward
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            print("Final Score: %s" % tot_reward)
+            tot_rewards.append(tot_reward)
+            break
+
+print "random agent average: %s" % (sum(tot_rewards)/float(len(tot_rewards)))
